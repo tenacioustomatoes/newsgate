a4574a0fdbb77e05eef3dac9d09775b9f04cf1f3
Autumn Smith <autumn07@gmail.com>
2016-12-14 10:33:27 -0800

    added changes to bias controller

--- client/app/bias/bias.js
+++ client/app/bias/bias.js
@@ -10,12 +10,6 @@ angular.module('newsgate.bias', [])
 .controller('BiasController', function($scope, $rootScope, $http, Data) {
   $scope.biasResult = '';
 
-  $http.get('/bias')
-    .then(function(res) {
-      $scope.biasData = res.data[0];
-      console.log($scope.biasData);
-    });
-
   var rating = {
     '0': "Far Left",
     '1': 'Left',
@@ -26,20 +20,15 @@ angular.module('newsgate.bias', [])
 
   //should be fired when query is set off
   $scope.searchBias = function(url) {
-    //checks url against urls in biased list
-    console.log('url',url);
-    // for (var data in $scope.biasData) {
-    //   console.log(data);
-    //   if (data === $scope.biasData) {
-    //     //if url found in data, return scoring
-    //     console.log(rating[data[$scope.biasData]]);
-    //     return rating[data[$scope.biasData]];
-    //   }
-    // }
+    url = 'cnn.com';
+    $http.get('/bias')
+      .then(function(res) {
+        $scope.biasData = res.data[0];
+        console.log('bias data',$scope.biasData);
+        if ($scope.biasData[url]) {
+          console.log('rating',rating[$scope.biasData[url]]);
+        }
+      });
   };
-
-  //call the function with the url to return rating
-  $scope.searchBias('cnn.com');
-  console.log($scope.biasResult);
 
 });
