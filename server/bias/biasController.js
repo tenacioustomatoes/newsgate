0e84009079c48c34cccbb83f697bac575a7e0f64
Justin Potter <juspotter@gmail.com>
2016-12-14 12:30:42 -0800

    Improved bias controller to compond result with response

--- server/bias/biasController.js
+++ server/bias/biasController.js
@@ -1,7 +1,46 @@
 var biasData = require('./biasrating.json');
+var url = require('url');
 
 module.exports = {
   getData: function (req, res, next) {
-    res.send(JSON.stringify(biasData));
+
+    var rating = {
+      '0': 'Far Left',
+      '1': 'Left',
+      '2': 'Center',
+      '3': 'Right',
+      '4': 'Far Right'
+    };
+
+
+    if (req.body.url) {
+      var domain = req.body.url.replace(/^https?:\/\//, ''); // replace http and https
+      domain = domain.replace(/www.?/, ''); //replace www. or www
+
+      domain = domain.split('/')[0]; //Get the domain and only the domain
+
+      if (domain) {
+        var biasResult = (biasData[0][domain] === undefined) ? null : rating[biasData[0][domain]];
+
+        var response = {
+          'url': domain,
+          'status': 'OK',
+          'bias': biasResult
+        };
+
+        res.compoundContent['bias'] = response; // how does this work?
+        next();
+
+      } else {
+        res.sendJSONresponse(res, 404, {
+          'message': domain + ' is a malformed url'
+        });
+      }
+
+    } else {
+      res.sendJSON.response(res, 404, {
+        'message': 'no url in request'
+      });
+    }
   }
 };
