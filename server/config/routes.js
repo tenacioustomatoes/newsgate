716c01eb3d98e3cfc55196869e47246d1a074c1f
Autumn Smith <autumn07@gmail.com>
2016-12-15 10:29:24 -0800

    added bias to routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -16,7 +16,7 @@ module.exports = function (app, express) {
   just for talking to Watson and so forth.
 */
 
-  var apiArr = [expanderController.expandURL, newsController.isFakeNews, watsonController.getTitle, watsonController.getKeywords, twitterSearch.getTweetsOnTopic, googleTrends.getGoogleTrends];
+  var apiArr = [expanderController.expandURL, newsController.isFakeNews, watsonController.getTitle, watsonController.getKeywords, twitterSearch.getTweetsOnTopic, googleTrends.getGoogleTrends, biasController.getData];
 
   app.post('/api', apiArr, function(req, res, next) {
     res.json(res.compoundContent);
