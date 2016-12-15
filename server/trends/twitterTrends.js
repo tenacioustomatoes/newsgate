0317037bce63a016c0a305a564cb24b7abe6cb35
Justin Potter <juspotter@gmail.com>
2016-12-14 18:43:38 -0800

    Fixed bug in routes, changed twitter to search based on keywords from watson

--- server/trends/twitterTrends.js
+++ server/trends/twitterTrends.js
@@ -15,7 +15,35 @@ twitter = Promise.promisifyAll(twitter);
 exports.getTweetsOnTopic = function(req, res, next) {
   // var keywords = res.compoundContent.title.title.slice(0,20);
   // console.log('twitter search titel: ', typeof res.compoundContent.title.title);
-  twitter.getAsync('search/tweets', {q: res.compoundContent.title.title, result_type: 'popular', count: 100})
+
+  var _filterKeywords = function(keywords) {
+    var outputKeywords = [];
+    outputKeywords = keywords.slice(0, 2);
+    outputKeywords = outputKeywords.map(keyword => {
+      var newkeyword = {};
+      newkeyword.relevance = keyword.relevance;
+      newkeyword.text = keyword.text.toLowerCase();
+      var solutations = /\b(m[rs]s*)\b\.*/gi;
+      newkeyword.text = newkeyword.text.replace(solutations, ''); //get rid of mr. and mrs.
+      newkeyword.text = newkeyword.text.trim();
+      console.log(keyword, 'in map keyword');
+      console.log(newkeyword, 'in map newkeyword');
+
+      return newkeyword;
+    });
+    //outputKeywords = keywords.filter(keyword => keyword.relevance > 0.75)
+    return outputKeywords;
+  };
+
+  console.log(_filterKeywords(res.compoundContent.keywords.keywords));
+  var query = '';
+  _filterKeywords(res.compoundContent.keywords.keywords).forEach(function(keyword) {
+    query += keyword.text + ' ';
+  });
+
+  console.log('query', query);
+
+  twitter.getAsync('search/tweets', {q: query, result_type: 'popular', count: 10})
   .then(function(data) {
     console.log(data);
     res.compoundContent['twitter'] = data;
